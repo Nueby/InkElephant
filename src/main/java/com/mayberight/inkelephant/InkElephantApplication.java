73f818113cc5698031839117fbf4a92f543b002d
ProngsSemper <798237844@qq.com>
2020-03-29 17:49:25 +0800

    Initial commit

---
+++ src/main/java/com/mayberight/inkelephant/InkElephantApplication.java
@@ -0,0 +1,13 @@
+package com.mayberight.inkelephant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkElephantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkElephantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mayberight/inkelephant/InkElephantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayberight.inkelephant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkElephantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
